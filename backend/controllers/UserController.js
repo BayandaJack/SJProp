0b48720612699bcc07838f17024c266d8444fa73
BayandaJack <2669074@students.wits.ac.za>
2025-11-01 20:35:03 +0200

    feat(user): Add update controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -12,6 +12,15 @@ const getAllUsers = async (req, res) => {
 
 
 //get a single user
+const getUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const User = await user.findById(id);
+        res.status(200).json(User);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 
 //create a new user
@@ -32,9 +41,28 @@ const createUser = async (req, res) => {
 }
 
 //update user details
+const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const updatedUser = await user.findByIdAndUpdate(id, data);
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 
 //delete a user
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const User = await user.findByIdAndDelete(id);
+        res.status(200).json(User);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
+}
 
 //export function for use elsewhere
-module.exports = { createUser, getAllUsers };
+module.exports = { createUser, getAllUsers, deleteUser, getUser, updateUser };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const { createUser, getAllUsers } = require('../controllers/UserController');
+const { createUser, getAllUsers, deleteUser, getUser, updateUser } = require('../controllers/UserController');
 
 //
 const router = express.Router();
 
 router.get('/', getAllUsers);
 
+router.get('/:id', getUser);
+
 router.post('/post', createUser);
 
-router.delete('/:id', (req, res) => {
-    res.json({msg: "Deleting from posts!"});
-});
+router.delete('/:id', deleteUser);
 
-router.patch('/:id', (req, res) => {
-    res.json({msg: "Updating posts!"});
-});
-
+router.patch('/:id', updateUser);
 
 module.exports = router;
