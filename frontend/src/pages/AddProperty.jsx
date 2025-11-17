2d52cd2851263a3cdfbaa6475629b7d288e37aef
BayandaJack <2669074@students.wits.ac.za>
2025-11-17 18:22:32 +0200

    refactor(AddProperty): Improve design of add property form

--- frontend/src/pages/AddProperty.jsx
+++ frontend/src/pages/AddProperty.jsx
@@ -1,7 +1,14 @@
+import PropDetailsForm from "../components/Forms/PropDetailsForm";
+
 export default function AddProperty(){
     return (
         <>
             <h1>Add Properties!!!</h1>
+            <section className="flex flex-row items-center justify-center">
+                <div className="W-3/4 bg-gray-200">
+                    <PropDetailsForm />
+                </div>
+            </section>
         </>
     );
 }
